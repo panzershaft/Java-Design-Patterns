bf119e22aa454c445276e66fd9284fe2fed7718e
panzershaft <contractor.soham@gmail.com>
2021-09-05 19:11:57 +0100

    Design-patterns: Singleton pattern

--- creational_patterns/src/singleton_pattern/Singleton.java
+++ creational_patterns/src/singleton_pattern/Singleton.java
@@ -31,6 +31,32 @@ ex: a single database object shared by different objects.
    to fail.
 
  */
-public class Runner {
+public class Singleton {
+    private static Singleton instance = null;
+    
+    private Singleton(){
+        System.out.println("SINGLETON-OBJECT CREATED\n");
+    }
+    
+    public static Singleton getInstance(){
+        if (instance == null) instance = new Singleton();
+        return instance;
+    }
 
+}
+
+class Runner {
+    public static void main(String[] args){
+        Singleton instance_one = Singleton.getInstance();
+        Singleton instance_two = Singleton.getInstance();
+        Singleton instance_three = Singleton.getInstance();
+
+        System.out.println("instance_one points to -> " + instance_one.hashCode());
+        System.out.println("instance_two points to -> " + instance_two.hashCode());
+        System.out.println("instance_three points to -> " + instance_three.hashCode());
+
+        if ( instance_one == instance_two && instance_two == instance_three)
+            System.out.println("\nAll three instances point to the same object");
+
+    }
 }
